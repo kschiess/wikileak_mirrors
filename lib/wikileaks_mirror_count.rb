00ed764d10b481caed95815b3b1ada83eacc9c99
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2010-12-08 16:53:46 +0100

    + Initial hack

---
+++ lib/wikileaks_mirror_count.rb
@@ -0,0 +1,29 @@
+
+require 'scrapi'
+require 'open-uri'
+
+# Scrapes the wikileaks mirror count from http://wikileaks.ch/mirrors.html.
+#
+class WikileaksMirrorCount
+  def initialize(url)
+    @url = url
+  end
+  
+  def get
+    wikileaks_mirrors = Scraper.define do
+      process "h3 + p", :text => :text
+
+      result :text
+    end
+
+    open(@url) do |html|
+      mirrors_text = wikileaks_mirrors.scrape(html.read)
+
+      if md=mirrors_text.match(/mirrored on (?<count>\d+) sites/)
+        return Integer(md[:count])
+      end
+    end
+    
+    fail "Could not retrieve the mirror count!"
+  end
+end
