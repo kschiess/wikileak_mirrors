5268be8b1dc1a0dea920ff5208d59d9b59f43dfc
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-12-08 18:23:31 +0100

    . initial implementation of mirror list

--- lib/mirror_list.rb
+++ lib/mirror_list.rb
@@ -1,25 +1,55 @@
 
+require 'yaml_store'
+
 # A persistent list of mirrors.
 #
 class MirrorList
+  # The backend store
+  attr_reader :store
+  
   # Create a mirror list instance. +store_dir+ is the directory where all
   # yaml cache files will be stored. +seed_url+ is the url that will be asked
   # for a mirror list if no existing list can be found in the store. 
   #
   def initialize(store_dir, seed_url)
+    @seed_url = seed_url
+    @store = YamlStore.new(store_dir)
+  end
+  
+  # Returns the current list
+  #
+  def list
+    @store.current || [@seed_url] 
   end
   
   # Refreshes all mirrors by asking one of the mirrors for an up to date list. 
   # This list is then stored and becomes the current list. 
   #
   def refresh
+    # Try as many urls from the list as needed to get a non nil result. 
+    new_list = retrieve
+    store.store new_list
   end
   
   # Implement Enumerable. If you want to be sure to have a list of mirrors,
   # whether or not a previous list has been retrieved, then call #refresh
   # before using the collection. 
   #
-  def each
+  def each(&block)
+    # list.each(&block)
   end
   include Enumerable
+  
+  # Tries as many mirrors as needed to get a valid result.
+  def retrieve
+    list.each do |url|
+      begin
+        return retrieve_mirrors(url)
+      rescue 
+        # Something bad happened - just continue trying all mirrors
+      end
+    end
+    
+    fail "Could not fetch a current list - none of the mirrors answered. Try reseeding."
+  end
 end
---
+++ spec/unit/mirror_list_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+require 'tempfile'
+
+require 'mirror_list'
+
+describe MirrorList do
+  attr_reader :temp_path
+  before(:each) do
+    tempfile = Tempfile.new('mirrorlist')
+    path = tempfile.path
+    tempfile.close(true)
+    
+    FileUtils.mkdir_p(path)
+    @temp_path = path
+  end
+  after(:each) do
+    FileUtils.rm_rf(@temp_path)
+  end
+  
+  let(:list) { MirrorList.new(temp_path, 'seed url') }
+  
+  context "when mirror store is empty" do
+    describe "<- #refresh" do
+      it "should retrieve mirrors from seed url" do
+        flexmock(list).
+          should_receive(:retrieve_mirrors).with('seed url').once
+          
+        list.refresh
+      end
+      it "should store current mirrors" do
+        flexmock(list).
+          should_receive(:retrieve_mirrors => :list)
+        list.refresh
+        
+        list.store.current.should == :list
+      end
+    end
+    describe "#each" do
+      it "should behave like an empty array" 
+    end
+  end
+end
