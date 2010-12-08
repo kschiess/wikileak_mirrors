829534d507b4a826801689289f2d978b601f891a
Florian Hanke <florian.hanke@gmail.com>
2010-12-08 23:07:13 +0100

    + move the scraper to an instance variable

--- lib/wikileaks_mirror_count.rb
+++ lib/wikileaks_mirror_count.rb
@@ -1,29 +1,28 @@
-
 require 'scrapi'
 require 'open-uri'
 
 # Scrapes the wikileaks mirror count from http://wikileaks.ch/mirrors.html.
 #
 class WikileaksMirrorCount
+  
   def initialize(url)
     @url = url
+    @wikileaks_mirrors = Scraper.define do
+      process "h3 + p", :text => :text
+      
+      result :text
+    end
   end
   
   def get
-    wikileaks_mirrors = Scraper.define do
-      process "h3 + p", :text => :text
-
-      result :text
-    end
-
     open(@url) do |html|
-      mirrors_text = wikileaks_mirrors.scrape(html.read)
-
-      if md=mirrors_text.match(/mirrored on (?<count>\d+) sites/)
+      mirrors_text = @wikileaks_mirrors.scrape(html.read)
+      
+      if md = mirrors_text.match(/mirrored on (?<count>\d+) sites/)
         return Integer(md[:count])
       end
     end
     
-    fail "Could not retrieve the mirror count!"
+    fail 'Could not retrieve the mirror count!'
   end
 end
