21f2f582aec81cc28f98267ba1b7343ee5822db1
Florian Hanke <florian.hanke@gmail.com>
2010-12-09 14:24:00 +0100

    + Renderer, + specs

---
+++ lib/renderer.rb
@@ -0,0 +1,45 @@
+require 'erb'
+require 'active_support'
+
+class Renderer
+  
+  attr_reader :template_path, :mirror_list
+  
+  def initialize template_path, mirror_list
+    @template_path = template_path
+    @mirror_list   = mirror_list
+  end
+  
+  # Refreshes the list of mirrors.
+  #
+  def refresh
+    mirror_list.refresh
+  end
+  
+  # Extracts a list of mirrors from the
+  #
+  def extract_list
+    mirror_list.to_a
+  end
+  
+  # Renders an index.html from the given template path ERB.
+  #
+  def render_into target_path
+    refresh
+    
+    # For the binding.
+    #
+    list = extract_list
+    
+    File.open(target_path, 'w') do |file|
+      file.write template.result binding
+    end
+  end
+  
+  # Returns a template
+  #
+  def template
+    ERB.new File.new(template_path).read, nil, '%'
+  end
+  
+end
---
+++ spec/unit/renderer_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'renderer'
+
+describe Renderer do
+  
+  it 'takes two parameters' do
+    lambda { Renderer.new :a, :b }.should_not raise_error
+  end
+  
+  before(:each) do
+    @mirror_list   = flexmock :mirror_list
+    @renderer      = flexmock Renderer.new('spec/fixtures/some_template.erb', @mirror_list)
+  end
+  
+  describe 'render_into' do
+    before(:each) do
+      @list = ['1', '2', '3']
+      @renderer.should_receive(:extract_list).and_return @list
+      
+      @renderer.should_receive(:refresh)
+    end
+    it 'should render' do
+      @renderer.render_into 'spec/fixtures/some_template.html'
+      
+      File.open('spec/fixtures/some_template.html', 'r') do |file|
+        lines = file.readlines
+        @list.include?(lines.shift.chomp).should == true
+        lines.shift.should == '3'
+      end
+    end
+  end
+  
+  describe 'refresh' do
+    it 'delegates' do
+      @mirror_list.should_receive(:refresh).once.with()
+      
+      @renderer.refresh
+    end
+  end
+  describe 'extract_list' do
+    it 'delegates' do
+      @mirror_list.should_receive(:to_a).once.with()
+      
+      @renderer.extract_list
+    end
+  end
+  describe 'template' do
+    it 'returns an ERB template' do
+      @renderer.template.kind_of?(ERB).should == true
+    end
+  end
+  
+end
