1cfcaa07bcfea00d8b9a74625c7345d60ef8fc54
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2010-12-10 12:13:39 +0100

    + a simple link checker

---
+++ lib/link.rb
@@ -0,0 +1,20 @@
+
+# A http link class that allows for simple link checking. 
+#
+class Link
+  attr_reader :url
+  def initialize(url)
+    @url = url
+  end
+  
+  def ok?
+    open(url) do |io|
+      return true
+    end
+    
+    # NEVER REACHED
+    return false
+  rescue
+    false
+  end
+end
---
+++ spec/unit/link_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+require 'link'
+
+describe Link do
+  let(:link) { Link.new('http://www.someurl.com') }
+  subject { link }
+  
+  describe "<- #ok?" do
+    context "when open fails" do
+      before(:each) { flexmock(link).should_receive(:open).and_raise('fail') }
+      subject { link.ok? }
+      it { should == false }
+    end
+    context "when open succeeds" do
+      before(:each) { flexmock(link).should_receive(:open).and_yield('ok') }
+      subject { link.ok? }
+      it { should == true }
+    end
+  end
+end
